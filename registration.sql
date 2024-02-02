08b841f8e1919aa9f9880a7cc54e43ff368473d6
Veerla-Dhana-Lakshmi <140573163+Veerla-Dhana-Lakshmi@users.noreply.github.com>
2024-02-02 16:42:42 +0530

    Create registration.sql

---
+++ registration.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Registration (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    DateOfBirth DATE
+    -- Add additional fields as needed
+);
